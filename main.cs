8b5e012c8ce602464eac1b5fe3dee39a7558d12c
MakingHistory2023 <143676019+MakingHistory2023@users.noreply.github.com>
2023-09-09 23:58:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("John Smith");
+    Console.WriteLine ("8/15/1978");
+    Console.WriteLine ("work 000-111-0101");
+    Console.WriteLine ("cell 000-111-0189");
+  }
+}
